a71e5f61c092bc09d038dfbe262a8d245c4073c7
scechin <sircechin@yahoo.com>
2020-06-27 16:15:25 +0000

    created variable clientLogo

--- src/js/views/NewTransactionInfo.js
+++ src/js/views/NewTransactionInfo.js
@@ -1,6 +1,7 @@
 import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.scss";
+import clientLogo from "../../img/logohoaBookkeeping.png";
 
 async function selectTransactionsToSend() {
 	console.log("Select transactions with missing information");
@@ -16,7 +17,7 @@ async function selectTransactionsToSend() {
 
 export const NewTransactionInfo = () => (
 	<div className="text-center mt-5">
-		<img src={logohoaBookkeeping}/>
+		<img src={clientLogo} />
 		<h1> Company X</h1>
 		<h2>Input Missing Transaction Information</h2>
 		<button onClick={selectTransactionsToSend}>Send to Client</button>
