1d85d883b5bb3a90602a63cbddf352cef53b7edf
scechin <sircechin@yahoo.com>
2020-07-13 22:05:35 +0000

    added some style

--- src/js/views/LoginToTransactionTracker.js
+++ src/js/views/LoginToTransactionTracker.js
@@ -11,28 +11,60 @@ export const LoginToTransactionTracker = () => {
 	const [email, saveEmail] = React.useState("");
 	const [password, savePassword] = React.useState("");
 	return (
-		<div className="text-center mt-5">
-			<img src={appLogo} />
+		<div className="container">
+			<div className="row">
+				<div className="col-sm-9 col-md-7 col-lg-5 mx-auto">
+					<div className="card card-signin my-5">
+						<div className="card-body">
+							<h5 className="card-title text-center">Login to track your transactions on Quickbooks</h5>
 
-			<h2>Login to track your transactions on Quickbooks</h2>
+							<img src={appLogo} />
 
-			<form>
-				<div className="row">
-					<div className="col">
-						<label>Enter Username</label>
-						<input type="text" onChange={e => saveEmail(e.target.value)} value={email} />
-					</div>
-					<div className="col">
-						<label>Enter Password</label>
-						<input type="text" onChange={e => savePassword(e.target.value)} value={password} />
+							<form className="form-signin">
+								<div className="form-label-group">
+									<label>Enter Email address</label>
+									<input
+										type="text"
+										id="inputEmail"
+										className="form-control"
+										placeholder="Email address"
+										required
+										onChange={e => saveEmail(e.target.value)}
+										value={email}
+									/>
+								</div>
+								<div className="form-label-group">
+									<label>Enter Password</label>
+									<input
+										type="password"
+										id="inputPassword"
+										className="form-control"
+										placeholder="Password"
+										required
+										onChange={e => savePassword(e.target.value)}
+										value={password}
+									/>
+								</div>
+								<div className="custom-control custom-checkbox mb-3">
+									<input type="checkbox" className="custom-control-input" id="customCheck1" />
+									<label className="custom-control-label">Remember password</label>
+								</div>
+								<button className="btn btn-lg btn-primary btn-block text-uppercase" type="submit">
+									Sign in
+								</button>
+								<hr className="my-4" />
+								<div clasName="row">
+									<div className="col">
+										<button onClick={() => actions.loginUser(email, password)}>
+											Connect to Quickbooks
+										</button>
+									</div>
+								</div>
+							</form>
+						</div>
 					</div>
 				</div>
-				<div clasName="row">
-					<div className="col">
-						<button onClick={() => actions.loginUser(email, password)}>Connect to Quickbooks</button>
-					</div>
-				</div>
-			</form>
+			</div>
 		</div>
 	);
 };
