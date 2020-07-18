397043cfc6f4ca909798b39183464a4127757ed7
scechin <sircechin@yahoo.com>
2020-07-18 00:33:33 +0000

    more about clients

--- src/js/views/Clients.js
+++ src/js/views/Clients.js
@@ -3,39 +3,23 @@ import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.scss";
 import clientLogo from "../../img/logohoaBookkeeping.png";
 
-async function selectTransactionsToSend() {
-	console.log("Select transactions with missing information");
+async function selectClients() {
+	console.log("Select Company/Client in Quickbooks");
 	const response = await fetch(process.env.API_HOST + "/clients", {
 		method: "GET"
 	});
 	const data = await response.json();
-	console.log("Select Clients in Quickbooks");
+	console.log("Select Company/Client in Quickbooks");
 }
 
-export const Transactions = () => (
+export const selectClient = () => (
 	<div className="text-center mt-5">
 		<img src={clientLogo} />
-
-		
-
-		
-
-		<button onClick={selectClients}>Select Clients</button>
 
 		<form>
 			<div clasName="row">
 				<div className="col">
-					<label>Company/Client</label>
-					<input type="text" />
-				</div>
-				<div className="col">
-					<label>Transaction id</label>
-					<input type="text" />
-				</div>
-			</div>
-			<div clasName="row">
-				<div className="col">
-					<button>Save Selected Transactions</button>
+					<button onClick={() => actions.selectClient(company_name)}>Select Company/Client</button>
 				</div>
 			</div>
 		</form>
