c190f4c215e3de76467168d12eb5520d6d10a776
scechin <sircechin@yahoo.com>
2020-07-24 20:19:24 +0000

    worked on navbar

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Context } from "../store/appContext.js";
+import appLogo from "../../img/transaction_tracker_logo_final.jpg";
 
 export const Navbar = () => {
 	const { store, actions } = React.useContext(Context);
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/home">
-				<h5>Transaction Tracker</h5>
+		<nav className="navbar navbar-expand-sm navbar-light bg-light mb-3">
+			<Link className="navbar-brand" to="/">
+				<img className="appLogoNav" src={appLogo} />
 			</Link>
 			<div className="ml-auto">
 				{store.token == null ? (
