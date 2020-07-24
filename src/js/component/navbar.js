4bbd458ff5355a11823ddadfdbd1e87f64d90dde
scechin <sircechin@yahoo.com>
2020-07-24 20:47:21 +0000

    fixed the navbar again

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Context } from "../store/appContext.js";
-import appLogo from "../../img/transaction_tracker_logo_final.jpg";
 
 export const Navbar = () => {
 	const { store, actions } = React.useContext(Context);
