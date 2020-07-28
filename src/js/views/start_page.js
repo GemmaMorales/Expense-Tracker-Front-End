316555021478c07c78c1e58216c8c3844634841c
scechin <sircechin@yahoo.com>
2020-07-28 20:36:22 +0000

    prepared the opening page

--- src/js/views/start_page.js
+++ src/js/views/start_page.js
@@ -11,22 +11,37 @@ export const StartPage = () => {
 	return (
 		<div className="container">
 			<div className="row">
-				<div className="col-sm-4 col-md-7 col-lg-5 mx-auto">
+				<div className="col-sm-4 col-md-7 col-lg-6 mx-auto">
 					<div className="card">
 						<div className="card-body">
+							<div className="row container">
+								<h1> </h1>
+							</div>
 							<h1 className="startpageh1">Welcome to Transaction Tracker</h1>
 							<div className="row container">
+								<h1> </h1>
+							</div>
+							<div className="row container">
 								<img className="appLogoStartPage" src={appLogo} />
+							</div>
+							<div className="row container">
+								<h5> </h5>
 							</div>
 							<div className="row container">
 								<h5 className="card-title text-center">
 									A DIRECT link between new transaction details and Quickbooks!
 								</h5>
 							</div>
-							<div className="row">
-								<Link to="/login" className="btn btn-outline-primary btn-sm btn-block text-capitalize">
-									Log In
+							<div className="row container">
+								<h5> </h5>
+							</div>
+							<div className="linkbtn">
+								<Link to="/login" className="btn btn-outline-primary btn-sm text-capitalize">
+									<h5>Log In</h5>
 								</Link>
+							</div>
+							<div className="row container">
+								<h5> </h5>
 							</div>
 						</div>
 					</div>
