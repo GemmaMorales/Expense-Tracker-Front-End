51819d88ddb57a9f8397351612594c14f5e932df
scechin <sircechin@yahoo.com>
2020-07-28 21:26:05 +0000

    navbar

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -7,9 +7,10 @@ export const Navbar = () => {
 	const { store, actions } = React.useContext(Context);
 	return (
 		<nav className="navbar navbar-expand-sm navbar-light bg-light mb-3">
-			<Link className="navbar-brand" to="/" login>
+			<Link className="navbar-brand" to="/start_page">
 				<img className="appLogoNav" src={appLogo} />
 			</Link>
+
 			<div className="ml-auto">
 				{store.token == null ? (
 					<Link to="/login">
