5443698196c70a85765590d834b923b0831668f2
scechin <sircechin@yahoo.com>
2020-07-29 18:37:55 +0000

    adjusting buttons

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -7,23 +7,27 @@ export const Navbar = () => {
 	const { store, actions } = React.useContext(Context);
 	return (
 		<nav className="navbar navbar-expand-sm navbar-light bg-light mb-3">
-			<Link className="navbar-brand" to="/start_page">
+			<Link className="navbar-brand" to="/">
 				<img className="appLogoNav" src={appLogo} />
 			</Link>
 
-			<div className="ml-auto">
+			<div className="ml-auto d-flex">
 				{store.token == null ? (
 					<Link to="/login">
-						<button className="btn btn-primary">Log in</button>
+						<button className="btn btn-primary">
+							<h5 className="whiteh5">Log in</h5>
+						</button>
 					</Link>
 				) : (
-					<div className="d-flex">
-						<Link to="/private">
-							<h5>Private</h5>
-						</Link>
+					<div className="container">
+						<button className="btn btn-outline-primary">
+							<Link to="/private">
+								<h5>Private</h5>
+							</Link>
+						</button>
 
-						<button className="btn btn-primary" onClick={() => actions.destroySession()}>
-							Log Out
+						<button className="btn btn-outline-primary" onClick={() => actions.destroySession()}>
+							<h5>Log Out</h5>
 						</button>
 					</div>
 				)}
